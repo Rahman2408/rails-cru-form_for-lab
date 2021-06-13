244e66b43c4aca082ead190d34d6d8f99e6904a3
Rahman Elsirafy <rahman2408@gmail.com>
2021-06-13 04:57:21 -0400

    commit tree f2877c3c5db70afc51bbe510d7c8cc262a226371

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+    before_action :set_song, only: [:show, :edit, :update]
+
+    def index
+       @songs = Song.all 
+    end
+
+    def new
+       @song = Song.new
+    end
+
+    def create
+       @song = Song.create(song_params(:name, :artist_id, :genre_id))
+        redirect_to song_path(@song)
+    end
+
+    def show 
+         @genre = @song.genre
+         @artist = @song.artist
+    end
+
+    def edit
+    end
+
+    def update
+        @song.update(song_params(:name, :artist_id, :genre_id))
+        redirect_to song_path(@song)
+    end
+
+private 
+    def song_params(*args)
+        params.require(:song).permit(*args)
+    end
+
+    def set_song
+        @song = Song.find(params[:id])
+    end
+end
